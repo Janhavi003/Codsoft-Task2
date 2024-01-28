17053f5e7077a9acadcbb181e387d4da15115638
Janhavi003 <janhavi.hivarekar@kalvium.community>
2024-01-28 18:59:22 +0530

    Task-2 completed

---
+++ ReadMe.md
@@ -0,0 +1,28 @@
+# CodSoft Internship
+
+<b>LEVEL1 :</b>
+
+<b>Task2: Portfolio</b>
+
+I have created my portfolio using HTML, CSS and Javascript. 
+I have added header section with name and logo/image.
+Also there is a About Section: with my image and bio highlighting my skills and experience.
+I have also added Projects Section: Showcase samples of my work or projects.
+The Contact Section includes my contact information, such as email address, linkedIn and Github address.
+Footer section includes a copyright notice.
+Screenshots for the Page:
+
+![Alt text](assets/Portfolio.png)
+
+<i>Caption: Home for Portfolio</i>
+
+
+![Alt text](assets/Portfolio-about.png)
+
+<i>Caption: About me section for Portfolio</i>
+
+
+
+![Alt text](assets/Portfolio-footer.png)
+
+<i>Caption: Projects and contact me for Portfolio</i>
---
+++ app.js
@@ -0,0 +1,78 @@
+// functionality for the typing section in header.
+var typed = new Typed('#element', {
+    strings: ['Web developer.', 'Software Developer.','UX/UI designer.'],
+    typeSpeed: 50,
+});
+// Calling all the variables.
+const codeQuiz = document.getElementById("codeQuiz")
+const codeRecipie = document.getElementById("codeRecipie")
+const codeZombie = document.getElementById("codeZombie")
+const codeTodo = document.getElementById("codeTodo")
+const codeCounter = document.getElementById("codeCounter")
+const codePy = document.getElementById("codePy")
+const liveQuiz = document.getElementById("liveQuiz")
+const liveRecipie = document.getElementById("liveRecipie")
+const liveZombie = document.getElementById("liveZombie")
+const liveTodo = document.getElementById("liveTodo")
+const liveCounter = document.getElementById("liveCounter")
+const livePy = document.getElementById("livePy")
+const footer2 = document.getElementById("footer2")
+const footer3 = document.getElementById("footer3")
+const footer4 = document.getElementById("footer4")
+// assigning onClick functioning.
+codeQuiz.onclick = ()=>{
+    window.location.href = "https://github.com/Janhavi003/FewdCA2-Quiz";
+    
+}
+codeRecipie.onclick = ()=>{
+    window.location.href = "https://github.com/Janhavi003/FEWD-CA-3-";
+    
+}
+codeZombie.onclick = ()=>{
+    window.location.href = "https://github.com/Janhavi003/zombie-shoot";
+    
+}
+codeTodo.onclick = ()=>{
+    window.location.href = " https://github.com/Janhavi003/lab-react-to-do-list";
+    
+}
+codeCounter.onclick = ()=>{
+    window.location.href = "https://github.com/Janhavi003/lab-react-counter-app";
+    
+}
+codePy.onclick = ()=>{
+    window.location.href = "https://github.com/Janhavi003/python-game";
+    
+}
+liveQuiz.onclick = ()=>{
+    window.location.href = "https://janhavi003.github.io/FewdCA2-Quiz/";  
+}
+liveRecipie.onclick = ()=>{
+    window.location.href = "https://janhavi003.github.io/FEWD-CA-3-/";
+    
+}
+liveZombie.onclick = ()=>{
+    window.location.href = "https://janhavi003.github.io/zombie-shoot/";
+    
+}
+liveTodo.onclick = ()=>{
+    window.location.href = "https://to-do-list-eight-lemon.vercel.app/";
+    
+}
+liveCounter.onclick = ()=>{
+    window.location.href = "https://lab-react-counter-app-opal.vercel.app/";
+    
+}
+footer2.onclick = ()=>{
+    window.location.href = "www.linkedin.com/in/janhavi-hivarekar-1b74a028b";
+    
+}
+footer3.onclick = ()=>{
+    window.location.href = "https://mail.google.com/mail/u/1/#inbox";
+    
+}
+footer4.onclick = ()=>{
+    window.location.href = "https://github.com/Janhavi003";
+    
+}
+// Javascript functionality ends here
